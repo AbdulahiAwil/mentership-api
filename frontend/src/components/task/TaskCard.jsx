73021f72624d50ec5d0410e5241b26874abef7f4
AbdulahiAwil <aamosman10@gmail.com>
2025-06-29 20:03:25 +0300

    button kii waab saxay

--- frontend/src/components/task/TaskCard.jsx
+++ frontend/src/components/task/TaskCard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Card,CardHeader,CardTitle,CardContent } from "@/components/ui/card"
-import { Button } from "@/components/ui/Button"
+import { Button } from "@/components/ui/button"
 import { Badge } from "@/components/ui/badge"
 import {
   DropdownMenu,
